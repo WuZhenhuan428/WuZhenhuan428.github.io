66b3c06b493a3cc0b92e723abb7c742d46aea8a9
WuZhenhuan <wuzhenhuan545@gmail.com>
2026-03-20 14:54:12 +0800

    2026-03-20

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1773308721040";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1773989607891";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
